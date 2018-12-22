71c53d8d59a6111510ddd485158db604ee4ea485
HUGGY1174 <45708034+HUGGY1174@users.noreply.github.com>
2018-12-22 16:55:50 +0900

    Ch05
    2018-12-22

---
+++ Ch05/Ex06.py
@@ -0,0 +1,8 @@
+alist = [1, 2, 3, 4]
+
+print("리스트 = [1, 2, 3, 4]")
+
+hap = alist[0] + alist[1] + alist[2] + alist[3]
+
+print("리스트 숫자들의 합" , hap)
+
---
+++ Ch05/Lab01.py
@@ -0,0 +1,18 @@
+frind_list = []
+
+a = input("친구의 이름을입력하시오.")
+frind_list.append(a)
+
+a = input("친구의 이름을입력하시오.")
+frind_list.append(a)
+
+a = input("친구의 이름을입력하시오.")
+frind_list.append(a)
+
+a = input("친구의 이름을입력하시오.")
+frind_list.append(a)
+
+a = input("친구의 이름을입력하시오.")
+frind_list.append(a)
+
+print(frind_list)
